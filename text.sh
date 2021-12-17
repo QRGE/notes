497d1ba01b4920e53df938c31705167d2b0d395a
QR <1826255833@qq.com>
2021-12-18 01:37:04 +0800

    update

--- text.sh
+++ text.sh
@@ -1,4 +1,1 @@
-https://images-gz.oss-cn-guangzhou.aliyuncs.com/temp/file_1639034783417.png,
-https://images-gz.oss-cn-guangzhou.aliyuncs.com/temp/logo_grey_1639034787793.png
-
-https://images-gz.oss-cn-guangzhou.aliyuncs.com/temp/logo_grey_1639034930075.png
+echo "test"
